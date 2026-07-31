5fd2432f08d85e62df6ef1f126a630424fa520b8
Shane Flandermeyer <shane.flandermeyer@ou.edu>
2026-07-31 15:43:50 -0500

    Disable mouse mode and add markview plugin

---
+++ lua/custom/plugins/markview.lua
@@ -0,0 +1,12 @@
+vim.pack.add({
+    "https://github.com/OXY2DEV/markview.nvim",
+})
+
+-- For `plugins/markview.lua` users.
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+};
